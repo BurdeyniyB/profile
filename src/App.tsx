3b0960757f0470470a0d5cc739f6160ba3ec9d47
Borys Burdeinyi <borys_burdeinyi@service-team.biz>
2026-05-19 17:33:32 +0300

    add tiled icon background, remove per-section backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Index from './pages'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TiledBackground from './components/TiledBackground';
 
 function App() {
 
   return (
     <>
+      <TiledBackground />
       <div className="header-wrapper">
         <Index />
       </div>
