d6f8406f405df84457a05de4d8080080e2491d50
Borys Burdeinyi <borys_burdeinyi@service-team.biz>
2026-05-20 14:09:31 +0300

    feat: register and mount map component in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { TiledBackgroundComponent } from './components/tiled-background/tiled-background.component';
-import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { ApproachComponent } from './components/approach/approach.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ProjectComponent } from './components/project/project.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppComponent } from "./app.component";
+import { TiledBackgroundComponent } from "./components/tiled-background/tiled-background.component";
+import { NavComponent } from "./components/nav/nav.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { ApproachComponent } from "./components/approach/approach.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ProjectComponent } from "./components/project/project.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MapComponent } from "./components/map/map.component";
 
 @NgModule({
   declarations: [
@@ -28,12 +29,9 @@ import { FooterComponent } from './components/footer/footer.component';
     ProjectComponent,
     ContactComponent,
     FooterComponent,
+    MapComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
